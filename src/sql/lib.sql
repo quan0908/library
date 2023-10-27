7a74ea7c723fa30b149657e14bb220dd73f36711
zyz19 <80141182@qq.com>
2023-10-27 19:18:09 +0800

    add user crud

--- src/sql/lib.sql
+++ src/sql/lib.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: lib
--- ------------------------------------------------------
--- Server version	8.0.29
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `blacklist`
---
-
-DROP TABLE IF EXISTS `blacklist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `blacklist` (
   `id` bigint NOT NULL COMMENT '黑名单id',
   `blackUserId` varchar(128) NOT NULL COMMENT '黑名单用户id',
@@ -30,24 +7,7 @@ CREATE TABLE `blacklist` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `blacklist`
---
-
-LOCK TABLES `blacklist` WRITE;
-/*!40000 ALTER TABLE `blacklist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blacklist` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `book`
---
-
-DROP TABLE IF EXISTS `book`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book` (
   `id` bigint NOT NULL COMMENT '图书id',
   `bookName` varchar(128) NOT NULL COMMENT '图书名',
@@ -59,24 +19,7 @@ CREATE TABLE `book` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `book`
---
-
-LOCK TABLES `book` WRITE;
-/*!40000 ALTER TABLE `book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `book_borrow_record`
---
-
-DROP TABLE IF EXISTS `book_borrow_record`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book_borrow_record` (
   `id` bigint NOT NULL COMMENT '图书借阅记录表',
   `userId` varchar(128) NOT NULL COMMENT '借阅用户id',
@@ -88,24 +31,7 @@ CREATE TABLE `book_borrow_record` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `book_borrow_record`
---
-
-LOCK TABLES `book_borrow_record` WRITE;
-/*!40000 ALTER TABLE `book_borrow_record` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book_borrow_record` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comments` (
   `id` bigint NOT NULL COMMENT '评论id',
   `content` text NOT NULL COMMENT '评论内容',
@@ -118,24 +44,7 @@ CREATE TABLE `comments` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `meeting_room`
---
-
-DROP TABLE IF EXISTS `meeting_room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `meeting_room` (
   `id` bigint NOT NULL COMMENT '会议室id',
   `name` varchar(128) NOT NULL COMMENT '会议室编号',
@@ -144,24 +53,7 @@ CREATE TABLE `meeting_room` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `meeting_room`
---
-
-LOCK TABLES `meeting_room` WRITE;
-/*!40000 ALTER TABLE `meeting_room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `meeting_room` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `meeting_room_borrow_record`
---
-
-DROP TABLE IF EXISTS `meeting_room_borrow_record`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `meeting_room_borrow_record` (
   `id` bigint NOT NULL COMMENT '会议室id',
   `userId` varchar(128) NOT NULL COMMENT '租借用户id',
@@ -172,55 +64,22 @@ CREATE TABLE `meeting_room_borrow_record` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `meeting_room_borrow_record`
---
-
-LOCK TABLES `meeting_room_borrow_record` WRITE;
-/*!40000 ALTER TABLE `meeting_room_borrow_record` DISABLE KEYS */;
-/*!40000 ALTER TABLE `meeting_room_borrow_record` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `user` (
-  `id` bigint NOT NULL COMMENT '用户表id',
+  `id` bigint COMMENT '用户表id' ,
   `account` varchar(128) NOT NULL COMMENT '账号',
-  `userName` varchar(128) NOT NULL COMMENT '姓名',
+  `username` varchar(128) NOT NULL COMMENT '姓名',
   `idCard` char(18) NOT NULL COMMENT '身份证号',
   `password` varchar(256) NOT NULL COMMENT '密码',
+  `userAvatar` varchar(256) COMMENT '用户头像',
   `foulTimes` int NOT NULL DEFAULT '0' COMMENT '违规次数',
-  `role` tinyint NOT NULL DEFAULT '0' COMMENT '-1-封号用户 0-普通用户 1-图书管理员 2-会议室管理员 3-超级系统管理员',
+  `role` varchar(64) NOT NULL DEFAULT '0' COMMENT 'BAN USER LIB_ADMIN MEETING_ROOM_ADMIN SUPPER_ADMIN',
   `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '0-不删 1删',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-16 17:11:08
