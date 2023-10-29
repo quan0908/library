d1bec4312b949463b85c98ff6582c4e015887ea1
quan <2923156890@qq.com>
2023-10-30 01:04:15 +0800

    增加Book字段，并修改了BookService

--- src/main/java/com/lib/constant/CommonConstant.java
+++ src/main/java/com/lib/constant/CommonConstant.java
@@ -16,5 +16,4 @@ public interface CommonConstant {
      * 降序
      */
     String SORT_ORDER_DESC = " descend";
-    
 }
---
+++ src/main/java/com/lib/model/entity/Book.java
@@ -0,0 +1,77 @@
+package com.lib.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName book
+ */
+@TableName(value ="book")
+@Data
+public class Book implements Serializable {
+    /**
+     * 图书id
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 图书名
+     */
+    private String bookName;
+
+    /**
+     * 图书数量
+     */
+    private Integer bookNumber;
+
+    /**
+     * 图书分类
+     */
+    private String type;
+
+    /**
+     * 图书位置
+     */
+    private String bookLocation;
+
+    /**
+     * 图书作者
+     */
+    private String bookAuthor;
+
+    /**
+     * 图书简介
+     */
+    private String bookTra;
+
+    /**
+     * 图书封面
+     */
+    private String bookCover;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    @TableLogic
+    /**
+     * 0-不删 1删
+     */
+    private Integer isDelete;
+
+
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
